6776f68d95ea752536b2c7486dac638e1309d3ea
John Du Hart <john@compwhizii.net>
2012-03-14 07:27:25 -0400

    Base framework for the library

---
+++ autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->registerNamespaces(array(
+	'GoodIRC' => __DIR__ . '/lib/GoodIRC'
+));
